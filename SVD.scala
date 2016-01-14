983841a86db292999d03173d664f1629290edd1d
parallelgithub <d97033@csie.ntu.edu.tw>
2016-01-14 11:58:05 +0800

    Start to TimeSVD: build time variable and function

--- SVD.scala
+++ SVD.scala
@@ -2,17 +2,18 @@
 
 import scala.io.Source
 import scala.util.Random
-//import util.control.Breaks._
 import scala.math
+import java.util.concurrent.TimeUnit
 
 		case class RatingDataStructure(userID: Int, movieID: Int, rating: Double, timestamp: Long)
 
-		val ratingFile = Source.fromFile("../dataset/ml-100k/u.data")
+		//val ratingFile = Source.fromFile("../dataset/ml-100k/u.data")
+		val ratingFile = Source.fromFile("../dataset/ml-1m/ratings.dat")
 			.getLines
 			.toList
 			.map{line =>
-				//val fields = line.split("::")
-				val fields = line.split("\t")
+				//val fields = line.split("\t")
+				val fields = line.split("::")
 				val tempRating = RatingDataStructure(fields(0).toInt, fields(1).toInt, fields(2).toDouble, fields(3).toLong)
 				tempRating
 				}
@@ -23,13 +24,13 @@ import scala.math
 		                 .reduceLeft( (a,b) => if (a.movieID > b.movieID) a else b) 
 		                 .movieID				
 		//number of factors in matrix factorization
-		val numFactors = 10
+		val numFactors = 2
 
 		val ratings = Array.fill(numUsers)(Array.fill(numMovies)(0.0))
 		ratingFile.foreach{ x => ratings(x.userID - 1)(x.movieID - 1) =  x.rating }
 
 		//隨機從numUsers個users中挑出testSize個test users
-		val testSize = 200
+		val testSize = (0.3 * numUsers).toInt
 		val userCandidate = List.range(1, numUsers+1)
 		def generateTestUsers(candidate: List[Int],count: Int, n: Int): List[Int] = {
 			if(count == 0)
@@ -88,6 +89,7 @@ class MatrixFacotrization extends TrainingModel {
 	val steps = 500
 	val alpha = 0.0002
 	val beta = 0.02		
+	//http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/
 
 	def predict(userIndex: Int, movieIndex: Int) = dotProduct(userIndex, movieIndex)
 
@@ -139,9 +141,11 @@ for(i <- 0 until n ) {
 class SVD extends TrainingModel {
 
 	val steps = 500
-	//??
-	//(0.001, 0.02) (0.01, 0.05) (0.015, 0.015)
+
 	val (gamma, lambda) = (0.002, 0.02)
+	//(0.001, 0.02) 
+	//(0.01, 0.05) http://blog.csdn.net/zhaoxinfan/article/details/8821419
+	//(0.015, 0.015) 2008 (SVD) A Guide to Singular Value Decomposition for Collaborative Filtering
 
 	val overallAverage = ratingFile.foldLeft(0.0)( (a,b) => a + b.rating) / ratingFile.size
 	//!! how to init
@@ -191,7 +195,6 @@ class SVD extends TrainingModel {
 	}
 
 	for(i <- 1 to 100) gradientDescent()
-
 	var last = math.abs(gradientDescent())
 	var loop = true
 	var i = 1
@@ -204,7 +207,7 @@ class SVD extends TrainingModel {
 		i += 1
 	}
 /*
-	var min = last
+	var min = math.abs(gradientDescent())
 	var step = 0
 	for(i <- 1 to steps){				
 		val err = math.abs(gradientDescent())
@@ -224,11 +227,11 @@ class SVD extends TrainingModel {
 class SVDPlus extends TrainingModel {
 
 	val nFB = numMovies 
-	//val w = numMovies
 	def w(value: Double): Double = value * 1000.0
 
-	val steps = 5
+	val steps = 100
 	val (gamma, lambda1, lambda2) = (0.007, 0.005, 0.015)
+	//(0.007, 0.005, 0.015) "Factorization meets the neighborhood- a multifaceted collaborative filtering model"
 
 	val overallAverage = ratingFile.foldLeft(0.0)( (a,b) => a + b.rating) / ratingFile.size
 	//!! how to init
@@ -336,6 +339,119 @@ class SVDPlus extends TrainingModel {
 */	
 }
 
+class TimeSVD extends TrainingModel {
+
+	val steps = 500
+
+	val (beta) = (0.4)
+	val (gamma, lambda) = (0.002, 0.02)
+
+	//!! how to init
+	val userDeviation = Array.fill(numUsers)(0.0)
+	val movieDeviation = Array.fill(numMovies)(0.0)
+	val ratedMovieOfUsers = ratings.map{ x => 
+		                        val s = x.size 
+		                        for(i <- 0 until s if x(i) > 0.0) 
+		                        	yield i 
+		                        }
+
+	val minStamp = ratingFile.reduceLeft( (a,b) => if (a.timestamp < b.timestamp) a else b).timestamp
+	val maxStamp = ratingFile.reduceLeft( (a,b) => if (a.timestamp > b.timestamp) a else b).timestamp
+	val numDays = days(maxStamp, minStamp) + 1
+	val times = Array.fill(numUsers)(Array.fill[Long](numMovies)(0))
+	//For the rating(u)(i) which we want to predict, its timestamp is preserved
+	ratingFile.foreach{ x => times(x.userID - 1)(x.movieID - 1) =  x.timestamp }
+	val userMeanDate = Array.tabulate(ratedMovieOfUsers.size) { u =>
+		val sum = ratedMovieOfUsers(u)
+		            .map{ i => days(times(u)(i), minStamp)}
+		            .reduceLeft(_+_)
+		if(ratedMovieOfUsers(u).size > 0)
+			sum.toDouble / ratedMovieOfUsers(u).size
+		else
+			globalMeanDate
+	}
+	//need to fix and the one in SVD++ SVD
+	val overallAverage = ratingFile.foldLeft(0.0)( (a,b) => a + b.rating) / ratingFile.size
+	val globalMeanDate = ratingFile.foldLeft(0.0)( (a,b) => a + days(b.timestamp, minStamp)) / ratingFile.size
+
+	def predict(userIndex: Int, movieIndex: Int) = { 
+		overallAverage + userDeviation(userIndex) + movieDeviation(movieIndex) + dotProduct(userIndex, movieIndex)
+
+	}
+
+	private def gradientDescent(): Double = {
+
+		for(u <- 0 until numUsers ; i <- 0 until numMovies){
+			if (ratings(u)(i) > 0){ //??
+				val stamp = times(u)(i)
+				val t = days(stamp, minStamp)
+				val binT = bin(t)
+				val dev_ut = dev(u, t)
+
+				val eui = ratings(u)(i) - predict(u,i)
+
+				userDeviation(u) += gamma * (eui - lambda * userDeviation(u))
+				movieDeviation(i) += gamma * (eui - lambda * movieDeviation(i))
+				for(h <- 0 until numFactors){
+					val puh = matrixP(u)(h)
+					matrixP(u)(h) += gamma * ( eui * matrixQ(h)(i) - lambda * matrixP(u)(h))
+					matrixQ(h)(i) += gamma * ( eui * puh - lambda * matrixQ(h)(i))
+				}
+			}
+		}
+		
+		var error = 0.0
+		for(u <- 0 until numUsers; i <- 0 until numMovies){
+			if (ratings(u)(i) > 0){ //??
+				val tempDot = ratings(u)(i) - predict(u,i)
+				error = error + tempDot * tempDot
+
+				//!! lambda
+				val bu2 = userDeviation(u) * userDeviation(u)
+				val bi2 = movieDeviation(i) * movieDeviation(i)
+				error = error + bu2 + bi2
+				for(h <- 0 until numFactors){					
+					val pu2 = matrixP(u)(h)*matrixP(u)(h)
+					val qi2 = matrixQ(h)(i)*matrixQ(h)(i)
+					
+					error = error + (lambda/2.0) * ( pu2 + qi2 )
+				}
+			}
+		}
+		error  
+	}
+
+	def days(d1: Long, d2: Long) = (TimeUnit.SECONDS.toDays(math.abs(d1 - d2))).toInt
+	def bin(day: Int) = (30.0 * day / numDays.toDouble ).toInt
+	def dev(u: Int, t: Int) = math.signum(t - userMeanDate(u)) * math.pow(math.abs(t - userMeanDate(u)), beta)
+/*
+	for(i <- 1 to 100) gradientDescent()
+	var last = math.abs(gradientDescent())
+	var loop = true
+	var i = 1
+	while(loop){
+		val err = math.abs(gradientDescent())
+		println("Training step " + i + " : error = " + err)
+		if(err > last)
+			loop = false
+		last = err
+		i += 1
+	}
+*/
+	var min = math.abs(gradientDescent())
+	var step = 0
+	for(i <- 1 to steps){				
+		val err = math.abs(gradientDescent())
+		println("Training step " + i + " : error = " + err)
+		if(err < min){
+			min = err
+			step = i
+		}
+	}
+	println("min error : " + min + " at step " + step)
+
+}
+
 		var mae: Double = 0.0
 		var maeCount: Int = 0
 
@@ -343,9 +459,18 @@ class SVDPlus extends TrainingModel {
 		
 		//Training
 		val matrix = select match {
-			case 1 => new MatrixFacotrization
-			case 2 => new SVD
-			case 3 => new SVDPlus
+			case 1 => 
+				println("Running matrix fatorization algorithm")
+				println
+				new MatrixFacotrization
+			case 2 => 
+				println("Running SVD algorithm")
+				println
+				new SVD
+			case 3 => 
+				println("Running SVD++ algorithm")
+				println
+				new SVDPlus
 		}
 
 		//def test(model: TrainingModel): Double { -1.0 }
@@ -375,31 +500,11 @@ class SVDPlus extends TrainingModel {
 		println("MAE = " + "%.3f".format(mae / maeCount) )
 
 /*
-Step = 5000
-MartrixFactorization
-beta = 0.02
-k = 2 : 0.757
-k = 3 : 0.761
-k = 4 : 0.755
-k = 5 : 0.793
-
-beta = 0.2
-k = 2 : 0.759
-k = 3 : 0.749
-k = 4 : 0.758
-k = 5 : 0.762
-
-SVD
-(gamma, lambda) = (0.0002, 0.02)
-f = 2 : 0.667
-
-SVD step=until local minimal
-(gamma, lambda) = (0.002, 0.02)
-testSize = 200
-f = 20
- MAE : 0.863 by 66892 steps in 7257.7 seconds 
-f = 2
- MAE : 0.707 by 530 steps
+For MovieLen 1m file with factor=2
+Matrix : 0.744
+SVD : 0.707
+SVD++ : 0.743 10 steps
+        0.727 50 steps
 
 SVD++
 
@@ -420,6 +525,19 @@ w: nFB
  0.738
 
 nFB用原來的定義 i.e. nFB=numMovies 
-w = numMovies 
+weight = numMovies 
+factor = 2
+ 0.797 10 steps
  0.745 15 steps
+ 0.827 20 steps
+ 0.805 30 steps
+ 0.778 50 steps
+ 0.827 100or150 steps
+ 0.722 100or150 steps
+ 0.825 200 steps
+
+factor = 10
+ 0.926 50 steps
+ 0.856 150 steps
+
 */		
